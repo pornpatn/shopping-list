1cb447cdc27c8f724defa54546b865a55773bbd1
Pornpat Nikamanon <pornpatn@gmail.com>
2023-05-24 09:17:56 -0700

    Update to match server

--- shopping-list-client/src/store/tagSlice.js
+++ shopping-list-client/src/store/tagSlice.js
@@ -1,9 +1,5 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
 import ChecklistAPI from '../api/checklistAPI';
-
-export const NEW_TAG_TEMPLATE = {
-    name: 'New TAG',
-};
 
 const initialState = {
     entities: [],
@@ -11,13 +7,14 @@ const initialState = {
     error: null,
 };
 
-export const fetchTagList = createAsyncThunk('tag/fetchTagList', async () => ChecklistAPI.fetchTags());
-
-export const createTag = createAsyncThunk('tag/createTag', async ({ data }) => ChecklistAPI.createTag(data));
-
-export const updateTag = createAsyncThunk('tag/updateTag', async ({ data }) => ChecklistAPI.updateTag(data));
-
-export const deleteTag = createAsyncThunk('tag/deleteTag', async ({ id }) => ChecklistAPI.deleteTag(id));
+export const fetchTagList = createAsyncThunk('tag/fetchTagList', async () => {
+    const response = await ChecklistAPI.fetchTags();
+    const result = await response.json();
+    if (!result.success) {
+        return isRejectedWithValue(result);
+    }
+    return result.data;
+});
 
 export const tagSlice = createSlice({
     name: 'tag',
@@ -27,15 +24,6 @@ export const tagSlice = createSlice({
         builder
             .addCase(fetchTagList.fulfilled, (state, action) => {
                 state.entities = action.payload;
-            })
-            .addCase(createTag.fulfilled, (state, action) => {
-                state.entities.push(action.payload);
-            })
-            .addCase(updateTag.fulfilled, (state, action) => {
-                state.entities = state.entities.map(entity => entity.id === action.payload.id ? action.payload : entity);
-            })
-            .addCase(deleteTag.fulfilled, (state, action) => {
-                state.entities = state.entities.filter(entity => entity.id !== action.payload);
             });
     }
 });
